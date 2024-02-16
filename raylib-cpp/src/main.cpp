c998547b4fa399bc1f2b8e17deb1f1d9611e58db
Menno <114731270+Buldz@users.noreply.github.com>
2024-02-16 13:46:28 +0100

    Update main.cpp

--- raylib-cpp/src/main.cpp
+++ raylib-cpp/src/main.cpp
@@ -1,50 +1,18 @@
-#include "raylib.h"
-
-constexpr auto SCREEN_WIDTH  = 800;
-constexpr auto SCREEN_HEIGHT = 450;
-
-int playerX = 400;
-int playerY = 225;
-int playerSpeed = 50;
+#include "config.h"
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "HELLPORTAL");
-    SetTargetFPS(60);
-
-    Texture2D test = LoadTexture("assets/test.png");
-
+    InitWindow(SWIDTH, SHEIGHT, "Maghar.IO");
+    SetWindowState(FLAG_VSYNC_HINT);
+    SetWindowState(FLAG_MSAA_4X_HINT);
+    // SetWindowState(FLAG_FULLSCREEN_MODE);
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
-        ClearBackground(BLACK);
+        ClearBackground(RAYWHITE);
         DrawFPS(5, 5);
-
-        DrawRectangle(playerX, playerY, 50, 50, BLUE);
-
         EndDrawing();
-
-        //ballX += 2;
-        if(IsKeyPressed(KEY_W))
-        {
-            playerY += -playerSpeed;
-        }
-        else if (IsKeyPressed(KEY_S))
-        {   
-            playerY += playerSpeed;
-        }
-        else if (IsKeyPressed(KEY_D))
-        {   
-            playerX += playerSpeed;
-        }
-        else if (IsKeyPressed(KEY_A))
-        {   
-            playerX += -playerSpeed;
-        }
     }
-
     CloseWindow();
-
     return 0;
 }
